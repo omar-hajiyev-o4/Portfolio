3ceeb1b436d38dc4f127b8b5105db3f063a7b5b8
omar-hajiyev-o4 <omer04118102@gmail.com>
2025-03-30 23:09:58 +0400

    Project is update

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,12 +1,6 @@
 export default {
   base: '/',
   build: {
-    assetsDir: 'static',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'static/[name].[hash][extname]',
-        entryFileNames: 'static/[name].[hash].js'
-      }
-    }
+    assetsDir: 'static'
   }
 }
